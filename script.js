47dedb8bad5abc7a7483cdb771d8d0ab11d1dee8
Shaun Yetman <shauny82@gmail.com>
2022-08-08 11:08:50 -0400

    added save button and function

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let countEl = document.getElementById("count-el")
-// let count = 0
+let count = 0
+let countEl = document.getElementById('count-el')
 
 function increment() {
     count = count + 1
     countEl.innerText = count
 }
 
+function save(){
+    console.log(count)
+}
